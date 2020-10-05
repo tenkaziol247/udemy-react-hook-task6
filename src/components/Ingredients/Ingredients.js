59334c63ea488621b9918f7a1be866b0246a92e2
tenkaziol247 <tenkaziol247@gmail.com>
2020-10-05 15:45:12 +0700

    lack custom hook

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -1,64 +1,99 @@
-import React, { useState, useEffect } from 'react';
+import React, { useCallback, useReducer } from 'react';
 
 import IngredientForm from './IngredientForm';
 import IngredientList from './IngredientList';
 import Search from './Search';
+import ErrorModal from '../UI/ErrorModal';
+
+const ingredientsReducer = (currentIngredients, action) => {
+  switch(action.type){
+    case 'SET':
+      return action.ingredients;
+    case 'ADD':
+      return [...currentIngredients, action.ingredient];
+    case 'DELETE':
+      return currentIngredients.filter(ingredient => ingredient.id !== action.id);
+    default: 
+      throw new Error('Should not get there!');
+  }
+};
+
+const httpReducer = (currentHttpState, action) => {
+  switch (action.type){
+    case 'STARTSEND':
+      return {loading: true, error: null};
+    case 'RESPONSE':
+      return { ...currentHttpState, loading: false};
+    case 'ERROR':
+      return { loading: false, error: action.errorMessage};
+    case 'CLEARERROR':
+      return { ...currentHttpState, error: null};
+    default: throw new Error('Should not reached');
+  }
+};
 
 const Ingredients = (props) => {
-  const [ userIngredients, setUserIngredients ] = useState([]);
+  // const [userIngredients, setUserIngredients] = useState([]);
+  const [ userIngredients, dispatch ] = useReducer(ingredientsReducer, []);
+  // const [ isLoading, setIsLoading ] = useState(false);
+  // const [ errorMessage, setErrorMessage ] = useState();
+  const [ httpState, dispatchHttp ] = useReducer(httpReducer, {});
 
-  useEffect(() => {
-    fetch(`https://udemy-react-hook-task6.firebaseio.com/ingredients.json`)
-      .then(response => {
-        return response.json();
-      })
-      .then(responseData => {
-        const loadedIngredients = [];
-        for(let key in responseData){
-          loadedIngredients.push({
-            id: key,
-            title: responseData[key].title,
-            amount: responseData[key].amount
-          });
-        };
-        setUserIngredients(loadedIngredients);
-      })
-      .catch(error => {
-        alert(error);
-      });
-  }, []);
+  // Nếu không có Search get data thì dùng useEffect để get data từ server về! Ở project này ta đã có Search nên không cần lấy data lần nữa!
 
   const addIngredientHandler = (ingredient) => {
+    dispatchHttp({type: 'STARTSEND'});
     fetch(`https://udemy-react-hook-task6.firebaseio.com/ingredients.json`, {
       method: 'POST',
       body: JSON.stringify(ingredient),
-      headers: { 'Content-Type': 'application/json'}
+      headers: { 'Content-Type': 'application/json' }
     }).then(response => {
       return response.json();
     }).then(responseData => {
-      setUserIngredients(prevIngredients => [
-        ...prevIngredients, 
-        {id: responseData.name,...ingredient}
-      ]);
+      // setUserIngredients(prevIngredients => [
+      //   ...prevIngredients,
+      //   { id: responseData.name, ...ingredient }
+      // ]);
+      dispatch({type: 'ADD', ingredient: { id: responseData.name, ...ingredient }});
+      dispatchHttp({type: 'RESPONSE'});
     }).catch(error => {
       alert(error);
-    })
-
+      dispatchHttp({type: 'ERROR', errorMessage: 'Something went wrong'});
+    });
   };
 
   const removeIngredientHandler = (ingredientId) => {
-    setUserIngredients(prevIngredients => {
-      return prevIngredients.filter((item) => item.id !== ingredientId);
-    })
+    dispatchHttp({type: 'STARTSEND'});
+    fetch(`https://udemy-react-hook-task6.firebaseio.com/ingredients/${ingredientId}.json`, {
+      method: 'DELETE'
+    }).then(response => {
+      // setUserIngredients(prevIngredients => {
+      //   return prevIngredients.filter((item) => item.id !== ingredientId);
+      // });
+      dispatch({type: 'DELETE', id: ingredientId});
+      dispatchHttp({type: 'RESPONSE'});
+    }).catch(error => {
+      dispatchHttp({type: 'ERROR', errorMessage: 'Something went wrong'});
+    });
+  };
+
+  const filteredIngredientsHandler = useCallback(filterdIngredients => {
+    dispatch({type: 'SET', ingredients: filterdIngredients});
+  }, []);
+
+  const clearError = () => {
+    dispatchHttp({type: 'CLEARERROR'});
   }
 
   return (
     <div className="App">
-      <IngredientForm onAddIngredient={addIngredientHandler}/>
+      {httpState.error && <ErrorModal onClose={clearError}>{httpState.error}</ErrorModal>}
+
+      <IngredientForm onAddIngredient={addIngredientHandler} loading={httpState.loading}/>
 
       <section>
-        <Search />
-        <IngredientList ingredients={userIngredients} onRemoveItem={removeIngredientHandler}/>
+        <Search onLoadIngredient={filteredIngredientsHandler} />
+        <IngredientList ingredients={userIngredients} onRemoveItem={removeIngredientHandler} />
       </section>
     </div>
   );
--- src/components/Ingredients/Search.js
+++ src/components/Ingredients/Search.js
@@ -1,15 +1,52 @@
-import React from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 
 import Card from '../UI/Card';
 import './Search.css';
 
 const Search = React.memo(props => {
+  const { onLoadIngredient } = props;
+  const [enteredFilter, setEnteredFilter] = useState('');
+  const inputRef = useRef();
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      if(enteredFilter === inputRef.current.value){
+        let queryCompare = enteredFilter.length === 0 ? '' : `?orderBy="title"&equalTo="${enteredFilter}"`;
+        fetch(`https://udemy-react-hook-task6.firebaseio.com/ingredients.json${queryCompare}`)
+          .then(response => {
+            return response.json();
+          })
+          .then(responseData => {
+            let loadedIngredients = [];
+            for(let key in responseData){
+              loadedIngredients.push({
+                id: key,
+                title: responseData[key].title,
+                amount: responseData[key].amount
+              })
+            };
+            onLoadIngredient(loadedIngredients);
+          })
+          .catch(error => {
+            alert(error);
+          });
+      }
+    }, 500);
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [enteredFilter, onLoadIngredient, inputRef]);
+
   return (
     <section className="search">
       <Card>
         <div className="search-input">
           <label>Filter by Title</label>
-          <input type="text" />
+          <input
+          ref={inputRef}
+            type="text"
+            value={enteredFilter}
+            onChange={event => setEnteredFilter(event.target.value)}/>
         </div>
       </Card>
     </section>
